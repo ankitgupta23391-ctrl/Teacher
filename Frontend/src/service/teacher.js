f9dfd33f887bb53f6e5802486d54e6511cf4188f
Ankit Gupta <ankitgupta23391@gmail.com>
2026-05-29 10:26:53 +0530

    added

--- Frontend/src/service/teacher.js
+++ Frontend/src/service/teacher.js
@@ -3,28 +3,28 @@ import { data } from 'react-router';
 
 
 export const addTeacher = async (data) => {
-    let response = await axios.post('http://localhost:3000/addteacher', data)
+    let response = await axios.post('https://teacher-2-wbvk.onrender.com/addteacher', data)
     return response;
 }
 
 export const allTeacher = async (data) => {
-    let response = await axios.get('http://localhost:3000/allteachers', data)
+    let response = await axios.get('https://teacher-2-wbvk.onrender.com/allteachers', data)
     return response;
 }
 
 
 
 export const deleteTeacher = async (id) => {
-  let response = await axios.delete(`http://localhost:3000/deleteteacher/${id}`);
+  let response = await axios.delete(`https://teacher-2-wbvk.onrender.com/deleteteacher/${id}`);
   return response;
 };
 
 
 export const getSingleTeacher = async (id) => {
-  let response = await axios.get(`http://localhost:3000/singleteacher/${id}`);
+  let response = await axios.get(`https://teacher-2-wbvk.onrender.com/singleteacher/${id}`);
   return response;
 };
 
 export const updateTeacher = async (id, data) => {
-  return await axios.patch(`http://localhost:3000/updateteacher/${id}`, data);
+  return await axios.patch(`https://teacher-2-wbvk.onrender.com/updateteacher/${id}`, data);
 };
